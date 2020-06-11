ebff09ac50e08ea72a570b82e038b356fef5258d
Ali Rostami <rostamiev@gmail.com>
2020-06-11 18:44:57 +0200

    the documentation is added

--- CGTeaApp.cpp
+++ CGTeaApp.cpp
@@ -7,6 +7,22 @@
 #include <wx/grid.h>
 #include "CGTeaSidebar.h"
 
+/*! \mainpage CGTea - A Brief Description.
+ * A mini-version of GTea written in C++ in which the view is based on wxWidget.
+ * The library part (Model) and the controllers need still a lot of improvements.
+ * For now, I am implementing the algorithms, reports, actions, ... based on the Java code.
+
+ * There are three main dependencies:
+ * - The Eigen library should be copied in the main directory.
+ * - wxWidget is also needed for the graphic-user interface. However, the library parts can be used without this library.
+ * - The boost library should be installed:
+     ```
+     $ sudo apt-get install build-essentials libboost-all-dev libmetis-dev metis
+     ```
+ * \image html image.png
+ * \section Extension
+ * \section Downloads
+*/
 
 bool CGTeaApp::OnInit()
 {
